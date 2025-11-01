44f139db0f4e9105cc00c6abf5b1e64ec739a523
prince nyarko <princenyarko360@gmail.com>
2025-11-01 18:59:55 +0000

    Modify admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,5 +4,6 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ("title", "author", "publication_year")
+    search_fields = ("title", "author")
 # Register the Book model 
 admin.site.register(Book, BookAdmin)
