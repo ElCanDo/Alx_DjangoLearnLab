fcad78ab0d2a6c7dc29db98535cc809c602996df
Prince Nyarko <Princenyarko360@gmail.com>
2025-11-04 00:52:58 +0000

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # List all books available in a Library.
-books = Library.objects.get(name=library_name, books.all())
+books = Library.objects.get(name='library_name')
 
 # Filter by a specific author
 books_by_author = Book.objects.filter(name="author_name")
