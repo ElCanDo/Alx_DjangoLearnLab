114f57c8d0c35e13427eb64fcaad2fb266059af1
prince nyarko <princenyarko360@gmail.com>
2025-11-07 18:58:33 +0000

    Delete query_sample.py

--- django-models/LibraryProject/relationship_app/query_sample.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# List all books available.
-books = Book.objects.all()
-
-# Filter by a specific author
-books_by_author = Book.objects.filter(author)
-
-# Query by 
