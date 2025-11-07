c6594bcaacedd4542eea569d0a854c2fb5db3aa5
prince nyarko <princenyarko360@gmail.com>
2025-11-07 21:06:26 +0000

    Modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Book, Library
 def book_list(request):
     books = Book.objects.all() #Fetch all books from database
     context = {'book_list': books} #Creates a context dictionary with book list
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
