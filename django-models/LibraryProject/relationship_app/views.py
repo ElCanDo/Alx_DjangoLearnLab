157a5c15d7ca2dcedc2465fdda0e3aa9e1e2d675
prince nyarko <princenyarko360@gmail.com>
2025-11-08 17:50:44 +0000

    Modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,13 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth.forms import UserCreationForm
+# from django.contrib import messages
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+# from django.views.generic import CreateView
 # Create your views here.
 
 def list_books(request):
@@ -23,13 +24,23 @@ class LibraryDetailView(DetailView):
 def index(request):
     return render(request, 'relationship_app/index.html')
 
-class RegisterView(CreateView):
-    """Handles user registration using Django's built-in UserCreationForm."""
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "relationship_app/register.html"
-    
+# class RegisterView(CreateView):
+#     """Handles user registration using Django's built-in UserCreationForm."""
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy("login")
+#     template_name = "relationship_app/register.html"
+
 def register(request):
+    """Handles user registration using Django's built-in UserCreationForm."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # messages.success(request, "Successful Registration! You can log in now.")
+            return redirect(reverse_lazy('login'))
+        else:
+            form = UserCreationForm()
+            return render(request, 'relationship_app/register.html', {'form':form})
     return RegisterView.as_view()(request)
 
 class LogoutView(LogoutView):
