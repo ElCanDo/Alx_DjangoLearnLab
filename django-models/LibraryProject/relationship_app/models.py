bde04214c94d8b4ca23f7e9fa3ab911a69a02ecf
prince nyarko <princenyarko360@gmail.com>
2025-11-08 21:30:42 +0000

    Modify models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -16,6 +16,13 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     def __str__(self):
         return self.name
+    
+    class Meta:
+        permissions = [
+            ('can_add_book', 
+            'can_change_book', 
+            'can_delete_book')
+        ]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -37,7 +44,9 @@ class UserProfile(models.Model):
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
 #Signal Decorator
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs): 
@@ -46,3 +55,5 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(
             user=instance,                       # Link to the new user instance
         )
+
+    
