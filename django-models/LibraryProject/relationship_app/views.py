93d4ae0b49c47fa8eca679dd6e372cb240a97c28
prince nyarko <princenyarko360@gmail.com>
2025-11-08 17:04:28 +0000

    Modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -28,6 +28,8 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "relationship_app/register.html"
+    def register(request):
+        return RegisterView.as_view()(request)
 
 class LogoutView(LogoutView):
     next_page = reverse_lazy('login')
