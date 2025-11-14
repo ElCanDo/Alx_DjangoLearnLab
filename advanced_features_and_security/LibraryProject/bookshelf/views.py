46c1886b69b1f32ec36021a91db142dd15abd5de
prince nyarko <princenyarko360@gmail.com>
2025-11-14 21:47:23 +0000

    Modify settings.py, models.py  and views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,29 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+
 def index(request):
     return HttpResponse("Welcome To My Library.")
 
-books = Book.objects.all()
-@permission_require('')
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return HttpResponse("Here you can create a book.")
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    return HttpResponse(f"Editing book with ID: {book_id}")
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    return HttpResponse(f"Deleting book with ID: {book_id}")
