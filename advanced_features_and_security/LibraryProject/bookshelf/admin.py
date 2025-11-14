2b4ed527e67becc2975492ac357188c5fa26f331
prince nyarko <princenyarko360@gmail.com>
2025-11-14 18:55:39 +0000

    Modify settings.py, admin.py and models.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
-from.models import CustomUser
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ("title", "author", "publication_year")
@@ -10,6 +10,13 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 
 class CustomUserAdmin(UserAdmin):
-    pass
-
+    model = CustomUser
+    #Visible fields when viewing a user
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields':('date_of_birth', 'profile_photo')}),
+                                     )
+    #Visible fields when creating a user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo'),})
+    )
 admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission
 
 
 # Create your models here.
@@ -15,10 +15,29 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
 
-# class CustomUserManager(BaseUserManager):
-#     class Meta:
-#         permisions = [
-#             ("create_user", "create user"),
-#             ("create_superuser", "create super user"),
-#          ]
+    def __str__(self):
+        return self.username
+    
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+        
+     
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.is_staff = True 
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        
+        
+
+        permissions = Permission.objects.filter(codename__in=['can_view', 'can_create', 'can_edit', 'can_delete'])
+        user.user_permissions.add(*permissions)
+
+        return user
