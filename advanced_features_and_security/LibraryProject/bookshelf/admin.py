6f9ece4d64d8fab0d1ad923bcf099007716cc7ae
prince nyarko <princenyarko360@gmail.com>
2025-11-14 16:22:40 +0000

    Modify settings.py, admin.py and models.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Book
-
+from django.contrib.auth.admin import UserAdmin
+from.models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ("title", "author", "publication_year")
     search_fields = ("title", "author")
 # Register the Book model 
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -12,11 +13,12 @@ class Book(models.Model):
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
-    profile_photo = models.ImageField(blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
 
-class CustomUserManager(BaseUserManager):
-    class Meta:
-        permisions = [
-            ("create_user", "create user"),
-            ("create_superuser", "create super user"),
-        ]
+# class CustomUserManager(BaseUserManager):
+#     class Meta:
+#         permisions = [
+#             ("create_user", "create user"),
+#             ("create_superuser", "create super user"),
+#          ]
+
