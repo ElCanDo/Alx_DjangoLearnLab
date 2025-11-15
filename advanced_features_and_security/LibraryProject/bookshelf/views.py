70d29ca867f38fcd94f4957061b43089fcf866ab
prince nyarko <princenyarko360@gmail.com>
2025-11-15 19:27:19 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from .forms import ExampleForm
 
 
 
