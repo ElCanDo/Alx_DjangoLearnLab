6863ccdb1619220c5ebb1f3f90f91ee8604d380c
prince nyarko <princenyarko360@gmail.com>
2025-11-21 22:31:12 +0000

    Add  serializers.py, urls.py and views.py

---
+++ api_project/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from api.views import BookListCreateAPIView
+
+urlpatterns = [
+    path("api/books", BookListCreateAPIView.as_view(), 
+         name="book_list_create"),
+]
