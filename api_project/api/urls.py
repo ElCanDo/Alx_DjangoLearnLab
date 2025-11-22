d8d5eb692b91fb195fa6002cb5bc35a58848279f
prince nyarko <princenyarko360@gmail.com>
2025-11-22 14:37:51 +0000

    Add .gitignore file, modify urls.py, views.py, and db.sqlite3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path,
-from api.views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
+    # Route for the BookList view (ListAPIView)
     path("api/books", BookList.as_view(), 
-         name="book_list_create"),
+         name='book_list'),
+    #The router URLs included for BookViewSet (all CRUD operations)
+    path('', include(router.urls)),
+    # This includes all routes registered with the router
 ]
