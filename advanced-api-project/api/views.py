1ddab760b4ebad53bdb0aae60e750bd3ab474d9f
prince nyarko <princenyarko360@gmail.com>
2025-11-27 19:15:55 +0000

    Modify views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics, filters
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,7 +11,7 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = IsAuthenticatedOrReadOnly, IsAuthenticated
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OderingFilter]
+    filter_backends = [ filters.SearchFilter, filters.OderingFilter]
     search_fields = []
     odering_fields = []
 
