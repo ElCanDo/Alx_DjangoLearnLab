c3be9035acf3289c1c3440e512539e1bec9086b7
prince nyarko <princenyarko360@gmail.com>
2025-11-27 20:50:09 +0000

    Modify test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -2,10 +2,19 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+
+
 class BookViewTests(APITestCase):
     def test_list_book(self):
-        url = reverse("book-list")
+        url = reverse("books")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
+    def test_create_book(self):
+        data = {"title":"ElCando's Journey", "publication_year": 2025, "author":"ElCanDo"}
+        url = reverse("books")
+        response = self.client.post(url, data)
+        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Optionally, assert on response.data if needed, e.g.:
+        self.assertEqual(response.data["title"], data["title"])
