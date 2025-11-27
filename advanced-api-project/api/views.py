6982b6eb605ca93bb4c2549dfb941b5ce2cbe9b6
prince nyarko <princenyarko360@gmail.com>
2025-11-27 20:01:54 +0000

    Add test_views.py and Modify views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework as filters
+from django_filters import rest_framework 
 from .models import Book
 from .serializers import BookSerializer
 
