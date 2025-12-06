afa1b48cfc6428c3315eea6a3be779efc7cb3d65
prince nyarko <princenyarko360@gmail.com>
2025-12-06 18:02:21 +0000

    Add Requirement.txt and Modify models.py with tag model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,3 +13,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    posts = models.ManyToManyField(Post, related_name='tags')
