f5c53421a9ad74d7c6722ae4c639fe5d209112fc
prince nyarko <princenyarko360@gmail.com>
2025-12-06 15:55:45 +0000

    Update app urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 post_urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("post/create/", views.PostCreateView.as_view(), name="post_create"),
+    path("post/new/", views.PostCreateView.as_view(), name="post_create"),
     path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 ] +  urlpatterns
