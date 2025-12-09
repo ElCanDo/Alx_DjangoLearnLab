6da83ed4ac46f691dff5ebbc03bbf584775a09d6
prince nyarko <princenyarko360@gmail.com>
2025-12-09 16:56:37 +0000

    Modify serializers.py file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,4 +3,6 @@ from rest_framework.authtoken.models import Token
 
 class RegistrationSerializer(serializers.ModelSerializer):
     serializer = serializers.CharField()
-    token = Token.objects.all()
+    token = Token.objects.create()
+
+    user = get_user_model().objects.create_user
