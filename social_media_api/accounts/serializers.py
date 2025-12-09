d07d77c210ffefc208853c42a3abbf3a91f1b05b
prince nyarko <princenyarko360@gmail.com>
2025-12-09 16:54:06 +0000

    Modify serializers.py file and urls.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,2 +1,6 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    serializer = serializers.CharField()
+    token = Token.objects.all()
