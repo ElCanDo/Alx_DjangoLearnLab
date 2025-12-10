cc02210bd221290ad9159ef1134633ab1552eaa3
prince nyarko <princenyarko360@gmail.com>
2025-12-10 18:11:13 +0000

    Initial set up for posts app and Modify settings.py to include the post app

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from accounts.models import CustomUser
+
+#Defining the Post Model
+class Post(models.Model):
+    authour = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+#Definining the Comment model
+class Comment(models.Model):
+    comment = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerilizer):
+    model = Post
+    fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    model = Comment
+    fields = '__all__'    
