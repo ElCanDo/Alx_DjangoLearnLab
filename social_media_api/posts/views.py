1b79c01d44ee2032b7cfe5d92baab7df6f30cfcf
prince nyarko <princenyarko360@gmail.com>
2025-12-10 18:29:22 +0000

    Modify views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import viewsets
+from .serializer import PostSerializer, CommentSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.iobjects.all()
+    serializer_class = CommentSerializer
