eed70bf3a5827de3cac021121ced08760be508cb
prince nyarko <princenyarko360@gmail.com>
2025-12-11 17:25:06 +0000

    modify views.py to include follow and unfollow views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import GenericAPIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 
+class FollowUserView(generics.GenericAPIView):
+    permission_class = [permissions.IsAuthenticated]
+
+
+class UnfolowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
