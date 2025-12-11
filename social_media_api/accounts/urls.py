ef4b2da47b4525c32649ace4fab9c89604d35894
prince nyarko <princenyarko360@gmail.com>
2025-12-11 20:24:54 +0000

    Modify urls.py to include profile urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-
+from .views import RegisterView, LoginView, ProfileView, FollowUserView, UnfollowUserView
 
 urlpatterns= [
-    path('login/', include())
-    path("register/", )
-    path("profile/", )
+     path('register/', RegisterView.as_view(), name='register'),
+        path('login/', LoginView.as_view(), name='login'),
+        path('profile/', ProfileView.as_view(), name='profile'),
+    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user"),
+    path("unfollow/<int:user_id>/",UnfollowUserView.as_view(), name="unfollow-user"),
+
 ]
+
