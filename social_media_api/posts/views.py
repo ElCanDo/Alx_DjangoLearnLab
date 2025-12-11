e2659bdb9c6b1bde83cc13388d91939a194b904d
prince nyarko <princenyarko360@gmail.com>
2025-12-11 21:01:33 +0000

    Modify views.py to include Feed views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .serializers import PostSerializer, CommentSerializer
 from .models import Post, Comment
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class FeedViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
-    
+
+    def get(self, request):
+        following_users = request.user.following.all()
+        feed_posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = self.get_serializer(feed_posts, many=True)
+        return Response(serializer.data)
+
 
 
 class CommentViewSet(viewsets.ModelViewSet):
