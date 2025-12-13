c43a6e2f66d783c435c24828e256679d418a23cd
prince nyarko <princenyarko360@gmail.com>
2025-12-13 16:44:37 +0000

    Modify views.py to implement post like and unlike

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, generics
 from .serializers import PostSerializer, CommentSerializer
-from .models import Post, Comment
+from .models import Post, Comment, Like
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 User = get_user_model()
 
@@ -10,6 +12,19 @@ User = get_user_model()
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        post = get_object_or_404(Post, pk=pk)
+        
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        if created:
+            return Response({'status': 'post liked'})
+        else:
+            like.delete()
+            return Response({'status': 'post unliked'})
+
 
 class FeedViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
@@ -20,7 +35,6 @@ class FeedViewSet(viewsets.ModelViewSet):
         feed_posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = self.get_serializer(feed_posts, many=True)
         return Response(serializer.data)
-
 
 
 class CommentViewSet(viewsets.ModelViewSet):
