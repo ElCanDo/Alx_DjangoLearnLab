257c39665fa02df472b01aa4d17ad266aa2e095d
prince nyarko <princenyarko360@gmail.com>
2025-12-14 20:08:28 +0000

    Preparation towards deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -138,3 +139,11 @@ X_FRAME_OPTIONS = 'DENY'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 SECURE_SSL_REDIRECT = True
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+import os
+
+PORT = os.environ.get("PORT", "8000") 
+
